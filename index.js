0fa62e134e3b95705aae4ca6248002ffd1cae4d8
fanlia <fanlia@outlook.com>
2019-07-04 16:01:05 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+const onFinished = require('on-finished')
+const Prometheus = require('prom-client')
+
+function requestDurationGenerator (buckets) {
+  return new Prometheus.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['route', 'method', 'status'],
+    buckets,
+  });
+}
+
+module.exports = (app, options = {}) => {
+
+    const {
+        metricsPath = '/metrics',
+        requestDurationBuckets = [ 1, 5, 10, 30, 60 ],
+        includeDefault = true,
+    } = options
+
+    app.get(metricsPath, (req, res) => {
+        res.set('Content-Type', Prometheus.register.contentType)
+        res.end(Prometheus.register.metrics())
+    })
+
+    app.locals.Prometheus = Prometheus
+
+    if (includeDefault) {
+        // Prometheus.collectDefaultMetrics()
+    }
+
+    const requestDuration = requestDurationGenerator(requestDurationBuckets)
+
+    const red = (req, res, next) => {
+
+        const end = requestDuration.startTimer()
+
+        onFinished(res, () => {
+
+            let { statusCode: status } = res
+            let { graphqlUrl, baseUrl: route, method } = req
+
+            if (graphqlUrl && route) route += graphqlUrl
+
+            route = route || req.path
+
+            end({route, method, status})
+        })
+
+        next()
+    }
+
+    app.use(red)
+
+}
